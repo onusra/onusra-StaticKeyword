6e6ca217f0aaa56c6a79552d8373cff3a194c77d
onusra <onusra@angelo.edu>
2023-06-22 00:06:10 +0000

    Static Completed

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -3,6 +3,8 @@
  * No need to change anything in this class. Its only job is to contain the static variable global_var.
  * See the Runner class for your task.
  */
+
+ 
 public class Class {
     static String global_var = "this variable is static";
 }
